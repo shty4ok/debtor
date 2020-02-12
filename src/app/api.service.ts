911091b8ce96671da304e8815495be5782f41b26
Aleksey <gurin.aleksey@gmail.com>
2020-02-12 11:09:14 +0300

    add environment

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,20 +2,22 @@ import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {DataDebtor} from "./data";
+import { environment } from "./environments/environments";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  apiUrl = environment.apiUrl;
+
   constructor(private httpClient: HttpClient) { }
 
   public getData(): Observable<DataDebtor> {
-    return this.httpClient.get<DataDebtor>(`http://localhost:3000/api/data`);
+    return this.httpClient.get<DataDebtor>(`${this.apiUrl}/api/data`);
   }
 
   public postData(dataSend: DataDebtor): Observable<DataDebtor> {
-    return this.httpClient.post<DataDebtor>(`http://localhost:3000/api/data`, dataSend, {responseType: 'json'});
+    return this.httpClient.post<DataDebtor>(`${this.apiUrl}/api/data`, dataSend, {responseType: 'json'});
   }
 }
-9
